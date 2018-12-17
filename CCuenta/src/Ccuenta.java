30ebb9318c4bf751e5f97a5238657bab5bf76493
Profe <root@usuarioEntDes>
2018-12-17 18:17:23 +0100

    Codigo corregido sin excepciones

--- CCuenta/src/Ccuenta.java
+++ CCuenta/src/Ccuenta.java
@@ -1,50 +1,41 @@
 
 public class Ccuenta {
 
+	// Atributos de cuenta
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInteres;
 
-	    // Atributos de cuenta
-	    private String nombre;
-	    private String cuenta;
-	    private double saldo;
-	    private double tipoInteres;
-
-
-
-	    public Ccuenta (String nom, String cue, double sal, double tipo)
-	    {
-	        nombre =nom;
-	        cuenta=cue;
-	        saldo=sal;
-	    }
-
-	    public double getSaldo ()
-	    {
-	        return saldo;
-	    }
-
-	    //Para probar Junit
-	    public void ingresar(double cantidad) throws Exception
-	    {
-	        if (cantidad<0)
-	            throw new Exception("Cantidad negativa");
-	        saldo = saldo + cantidad;
-	    }
-
-
-
-	    //Para probar JUnit
-	    public void retirar (double cantidad) throws Exception
-	    {
-	        if (cantidad < 0)
-	            throw new Exception ("Cantidad Negativa");
-	        if (this.getSaldo()< cantidad)
-	            throw new Exception ("Saldo insuficiente");
-	        saldo = saldo - cantidad;
-	    }
-
-	    // M�todo que me devuelve el n�mero de cuenta
-	    public String getCuenta ()
-	    {
-	        return cuenta;
-	    }
+	public Ccuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+		tipoInteres = tipo;
 	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	// Para probar Junit
+	public void ingresar(double cantidad) {
+		if (cantidad < 0)
+			System.out.println("Cantidad negativa");
+		saldo = saldo + cantidad;
+	}
+
+	// Para probar JUnit
+	public void retirar(double cantidad) {
+		if (cantidad < 0)
+			System.out.println("Cantidad Negativa");
+		if (this.getSaldo() < cantidad)
+			System.out.println("Saldo insuficiente");
+		saldo = saldo - cantidad;
+	}
+
+	// Método que me devuelve el número de cuenta
+	public String getCuenta() {
+		return cuenta;
+	}
+}
